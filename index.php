a67f86e3f569354a5d491bc690655d2d8a7c1c58
Abeullus <54740614+Abeullus@users.noreply.github.com>
2022-09-19 11:32:14 +0200

    new link

--- index.php
+++ index.php
@@ -40,16 +40,15 @@
         <div class="content">
             <img class="logo" src="./images/ur-logo-bildmarke-grau.png">
             <div class="content-inner">
-                <h1 class="h1">Willkommen zur Online-Studie <br>"vom Novizen zum Experten"</h1>
-                <p class="text" style="height: 4em">Die Studie besteht aus insgesamt <b> drei Fragebögen </b> und  <b> fünf Versuchsdurchläufen </b>, die Ihnen nacheinander gezeigt werden.</p>
+                <h1 class="h1">Willkommen zur Online-Studie "vom Novizen zum Experten" </h1>
+                <p class="text" style="height: 5em">Die Studie besteht aus insgesamt <b> drei Fragebögen </b> und  <b> fünf Versuchsdurchläufen </b>, die Ihnen nacheinander gezeigt werden. 
                 Sie werden bei jedem Fragebogen gebeten Ihre <b> Session-ID </b> einzugeben, diese befindet sich links unten im Balken. 
-
                 Die Studie dauert ca. 30 Minuten. Sofern benötigt , erhalten Sie am Ende nach erfolgreicher Teilname  <b> 0,5 VP </b> . Zudem gibt es eine Verlosung für einen 15€ Gutschein eurer Wahl.
-
                 <br>
-
-                Vielen Dank, dass Sie mir bei meiner Bachelorarbeit helfen!
-
+                <br> 
+                Vielen Dank, dass Sie mir bei meiner Bachelorarbeit helfen!</p>
+                <br>
+                <br>
 
                 <form action="./studies/questionnaire.php" method="POST">
                     <label style="display: block">
